e27001cb934b09fffd6075ddb5cb614a35368293
KagawaMinamo <69186578+KagawaMinamo@users.noreply.github.com>
2020-08-26 17:32:52 +0900

    20200826課題提出

--- 画像処理アプリ/画像処理アプリ/画像処理アプリ.py
+++ 画像処理アプリ/画像処理アプリ/画像処理アプリ.py
@@ -4,6 +4,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+import sys
 
 file_path = ''
 file_save_path = ''
@@ -13,7 +14,7 @@ def file_select(event):
   # 初期化
   txt_file_path.delete(0, tk.END)
   # 拡張子
-  fTyp = [("すべてのファイル", "*")]
+  fTyp = [('JPGファイル', '*.jpg'),('PNGファイル','*.png'),('GIFファイル','*.gif')]
   # 最初の表示するディレクトリ
   idir = 'C:\\Users'
   file_path = tk.filedialog.askopenfilename(filetypes = fTyp, initialdir = idir)
@@ -30,11 +31,10 @@ def file_save(event):
   fTyp = [("すべてのファイル", "*")]
   # 最初の表示するディレクトリ
   idir = 'C:\\Users'
-  file_save_path = tk.filedialog.asksaveasfilename(filetypes = fTyp, initialdir = idir)
-
+  file_save_path = tk.filedialog.asksaveasfilename(defaultextension = 'jpg', filetypes = fTyp, initialdir = idir)
   # ファイルパスが格納されていたらテキストボックスに表示
   if file_save_path:
-     file_save_path.insert(tk.END, file_path)
+     txt_file_save_path.insert(tk.END, file_save_path )
   else:
      return
 
@@ -44,6 +44,10 @@ def Img_Blur(event):
   # ファイルを選んでなかったらメッセージ出して処理終了
   if txt_file_path.get() is '':
     messagebox.showerror('エラー','画像ファイルを選択してください')
+    return
+
+  if txt_file_save_path.get() is '':
+    messagebox.showerror('エラー','保存先を選択してください')
     return
 
   #例外処理
@@ -71,9 +75,10 @@ def Img_Blur(event):
   except Exception as ex:
     messagebox.showerror('エラー', ex)
     return
-    
-  # できた画像をフォトで開く
-  clone_img.show()
+  
+  # 保存
+  clone_img.save(txt_file_save_path.get())
+  messagebox.showinfo('情報', 'ぼかし処理が完了しました')
 
 
 # モノクロ処理  
@@ -83,19 +88,56 @@ def Img_Monochrome(event):
     messagebox.showerror('エラー','画像ファイルを選択してください')
     return
 
+  if txt_file_save_path.get() is '':
+    messagebox.showerror('エラー','保存先を選択してください')
+    return
+
   img = Image.open(txt_file_path.get())
   # グレースケールに変換
   gray_img = img.convert('L')
-  # できた画像をフォトで開く
-  gray_img.show()
+  # 保存
+  gray_img.save(txt_file_save_path.get())
+  messagebox.showinfo('情報', 'モノクロ処理が完了しました')
 
+
+# 色反転処理
+def Img_RGB_Flip(event):
+  if txt_file_path.get() is '':
+    messagebox.showerror('エラー','画像ファイルを選択してください')
+    return
+
+  if txt_file_save_path.get() is '':
+    messagebox.showerror('エラー','保存先を選択してください')
+    return
+
+  img = Image.open(txt_file_path.get())
+  width, height = img.size
+  flip_img = Image.new('RGB', (width, height))
+  img_pixels = np.array([[img.getpixel((x,y)) for x in range(width)] for y in range(height)])
+
+  # 色を反転する
+  reverse_color_pixels = 255 - img_pixels
+  for y in range(height):
+    for x in range(width):
+      # 反転した色の画像を作成する
+      r,g,b = reverse_color_pixels[y][x]
+      flip_img.putpixel((x,y), (r,g,b))
+
+  flip_img.save(txt_file_save_path.get())
+  messagebox.showinfo('情報', '色反転処理が完了しました')
+
+
+def Close(event):
+    sys.exit(0)
+
+#-------------------------------------------------------------------------------------------------
 
 root = tk.Tk()
 root.title(u"画像処理アプリ")
 # ウィンドウのwidthとheight
 root.geometry("480x270")
 
-# ラベル
+# ファイル選択ラベル
 lbl_file_path = tk.Label(text=u'ファイル名')
 lbl_file_path.place(x=10, y=10)
 
@@ -108,9 +150,9 @@ btn_file_select = tk.Button(text=u'ファイル選択')
 btn_file_select.bind("<Button-1>", file_select) # クリックイベントにメソッドをバインド
 btn_file_select.place(x=360, y=35)
 
-# ラベル
+# 保存場所ラベル
 lbl_file_path = tk.Label(text=u'保存場所')
-lbl_file_path.place(x=10, y=60)
+lbl_file_path.place(x=10, y=65)
 
 # 処理対象ファイルパスを表示するテキストボックス
 txt_file_save_path = tk.Entry(width = 55)
@@ -124,11 +166,28 @@ btn_file_save_select.place(x=360, y=85)
 # ぼかし処理ボタン
 btn_blur = tk.Button(text=u'ぼかす')
 btn_blur.bind("<Button-1>", Img_Blur) 
-btn_blur.place(x=100, y=150)
+btn_blur.place(x=100, y=180)
 
 # モノクロ処理ボタン
 btn_mosaic = tk.Button(text=u'モノクロ')
 btn_mosaic.bind("<Button-1>", Img_Monochrome) 
-btn_mosaic.place(x=200, y=150)
+btn_mosaic.place(x=200, y=180)
+
+# 処理ボタン
+btn_blur = tk.Button(text=u'色反転')
+btn_blur.bind("<Button-1>", Img_RGB_Flip) 
+btn_blur.place(x=300, y=180)
+
+# 注意文言ラベル
+lbl_caution = tk.Label(text=u'●保存場所を選択するときに拡張子を入力しない！')
+lbl_caution.place(x=10, y=130)
+
+lbl_caution = tk.Label(text=u'●保存される画像はJPEG形式！')
+lbl_caution.place(x=10, y=150)
+
+# 閉じるボタン
+btn_close = tk.Button(text=u'閉じる')
+btn_close.bind("<Button-1>", Close) 
+btn_close.place(x=380, y=230)
 
 root.mainloop()
