546e72a4b6d48d07f2cf2d771ab042bc85edda9b
KagawaMinamo <69186578+KagawaMinamo@users.noreply.github.com>
2020-09-03 17:41:25 +0900

    20200903課題提出

--- 機械学習/教師あり学習アルゴリズム.py
+++ 機械学習/教師あり学習アルゴリズム.py
@@ -6,17 +6,18 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import mglearn
 from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
 
-# データセットの生成
-# forgeデータセットは二つの特徴量を持つ
-X, y = mglearn.datasets.make_forge()
-# データセットのプロット
-mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
-# 凡例の表示
-plt.legend(["Class 0", "Class 1"], loc = 4)
-plt.xlabel("First feature") # 第一特徴量
-plt.ylabel("Second feature") # 第二特徴量
-print("X.shape: {}".format(X.shape))
+# # データセットの生成
+# # forgeデータセットは二つの特徴量を持つ
+# X, y = mglearn.datasets.make_forge()
+# # データセットのプロット
+# mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+# # 凡例の表示
+# plt.legend(["Class 0", "Class 1"], loc = 4)
+# plt.xlabel("First feature") # 第一特徴量
+# plt.ylabel("Second feature") # 第二特徴量
+# print("X.shape: {}".format(X.shape))
 
 # waveで回帰アルゴリズム
 X, y = mglearn.datasets.make_wave(n_samples=40)
@@ -24,3 +25,16 @@ plt.plot(X, y, 'o')# 第3引数はオプションを表しており、今回はm
 plt.ylim(-3, 3)
 plt.xlabel("Feature")# 特徴量
 plt.ylabel("Target")# 回帰ターゲット
+
+# k-最近傍法によるクラス分類
+# 星印で示される3つの新しいデータポイント
+# 1-最近傍法での予測では近傍点のラベルが予測されたラベルになる
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+# 個々のテストする点に対して、近傍点のうちいくつがクラス0に属し、いくつがクラス1に属すか計算し、多いほうのクラスをその点に与える
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+# 1-最近傍回帰を用いたデータセット予測
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+# 複数の最近傍点を用いる場合は最近傍点の平均値を用いる
+# 3-最近傍回帰を用いたデータセット予測
+mglearn.plots.plot_knn_regression(n_neighbors=3)
--- 機械学習/腫瘍の悪性予測.py
+++ 機械学習/腫瘍の悪性予測.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 import mglearn
 from sklearn.datasets import load_breast_cancer
 from sklearn.datasets import load_boston
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 
 # 腫瘍データをロードする
 cancer = load_breast_cancer()
@@ -29,10 +31,69 @@ X, y = mglearn.datasets.load_extended_boston()
 print("X.shape: {}".format(X.shape))
 
 #--------------------------------------------------------------------------------------------------------------------
-# k-最近傍法によるクラス分類
-# 星印で示される3つの新しいデータポイント
-# 1-最近傍法での予測では近傍点のラベルが予測されたラベルになる
-mglearn.plots.plot_knn_classification(n_neighbors=1)
-# 個々のテストする点に対して、近傍点のうちいくつがクラス0に属し、いくつがクラス1に属すか計算し、多いほうのクラスをその点に与える
-mglearn.plots.plot_knn_classification(n_neighbors=3)
+# # データを訓練セットとテストセットに分割
+# X, y = mglearn.datasets.make_forge()
+# X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=0)
+# # インスタンスの生成
+# clf = KNeighborsClassifier(n_neighbors=3)
+# # 訓練セットを用いてクラス分類器を訓練(データセット保存)
+# clf.fit(X_train, y_train)
+# # テストデータに対して予測
+# print("Test set prediction: {}".format(clf.predict(X_test)))
+# # テストの評価
+# print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
 
+# # K-近傍法の解析
+# fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+# #kが1,3,9の時のクラス0の割り当てる場合と、クラス1に割り当てる場合の決定境界を描画
+# for n_neighbors, ax in zip([1, 3, 9], axes):
+#     #fitメソッドは自分自身を返すので、1行でインスタンスを生成してfitすることができる
+#     clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+#     mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+#     mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+#     ax.set_title("{} neighbor(s)".format(n_neighbors))
+#     ax.set_xlabel("feature 0")
+#     ax.set_ylabel("feature 1")
+# axes[0].legend(loc=3)
+
+#------------------------------------------------------------------------------------------------------------------------
+# モデルの複雑さと汎化性能の関係を確認
+# データセットを訓練セットとテストセットに分割
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+# neighborsを1から10まで試す
+neighbors_settings = range(1, 11)
+
+# 訓練セットに対する精度とテストセットに対する性能を近傍点の数に対して評価
+for n_neighbors in neighbors_settings:
+    # モデルを構築
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    # 訓練セット制度を記録
+    training_accuracy.append(clf.score(X_train, y_train))
+    # 汎化制度を記録
+    test_accuracy.append(clf.score(X_test, y_test))
+    
+# 結果を図に表示
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+
+#--------------------------------------------------------------------------------------------------------------------------
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+# waveデータセットを訓練セットとテストセットに分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+# モデルのインスタンス作成
+reg = KNeighborsRegressor(n_neighbors=3)
+# 訓練データと訓練ターゲットを用いてモデルを学習させる
+reg.fit(X_train, y_train)
+# テストセットに対して予測
+print("Test set predictions:\n{}".format(reg.predict(X_test)))
+# モデルの評価
+# R^2スコアは決定係数とも呼ばれ、回帰モデルの予測の正確さを測る指数で0から1までの値を取る
+print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
