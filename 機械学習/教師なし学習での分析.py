b0d273c61ce6f259b0b5f3be22a6802a88d9ffc4
KagawaMinamo <69186578+KagawaMinamo@users.noreply.github.com>
2020-09-24 17:33:18 +0900

    20200924課題

--- 機械学習/教師なし学習での分析.py
+++ 機械学習/教師なし学習での分析.py
@@ -4,6 +4,50 @@ import pandas as pd
 # from IPython.display import display
 from sklearn.model_selection import train_test_split
 import mglearn
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import MinMaxScaler # スケール変換器
 
 # 様々なスケール変換と前処理結果
 mglearn.plots.plot_scaling()
+
+#-------------------------------------------------------------------------------------------------
+# 訓練データとテストデータを同じように変換
+# m合成データを作成
+X, _ = make_blobs(n_samples=50, centers=5, random_state=4, cluster_std=2)
+# 訓練セットとデータセットに分割
+X_train, X_test = train_test_split(X, random_state=5, test_size=.1)
+
+# 訓練セットとテストセットをプロット
+fig, axes = plt.subplots(1, 3, figsize=(13, 4))
+axes[0].scatter(X_train[:, 0], X_train[:, 1], c=mglearn.cm2(0), label="Training set", s=60)
+axes[0].scatter(X_test[:, 0], X_test[:, 1], marker='^', c=mglearn.cm2(1), label="Test set", s=60)
+axes[0].legend(loc='upper left')
+axes[0].set_title("Original Data")
+
+# MinMaxScalerでデータをスケール変換
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# スケール変換されたデータの特性を可視化
+axes[1].scatter(X_train_scaled[:, 0], X_train_scaled[:, 1], c=mglearn.cm2(0), label="Training set", s=60)
+axes[1].scatter(X_test_scaled[:, 0], X_test_scaled[:, 1], marker='^', c=mglearn.cm2(1), label="Test set", s=60)
+axes[1].set_title("Scaled Data")
+
+# テストセットを訓練セットとは別にスケール変換
+# 最小値と最大値が0,1になる
+# 実際にはやってはいけない
+test_scaler = MinMaxScaler()
+test_scaler.fit(X_test)
+X_test_scaled_badly = test_scaler.transform(X_test)
+
+# 間違ってスケール変換されたデータを可視化
+axes[2].scatter(X_train_scaled[:, 0], X_train_scaled[:, 1], c=mglearn.cm2(0), label="training set", s=60)
+axes[2].scatter(X_test_scaled_badly[:, 0], X_test_scaled_badly[:, 1], marker='^', c=mglearn.cm2(1), label="test set", s=60)
+axes[2].set_title("Improperly Scaled Data")
+
+for ax in axes:
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+fig.tight_layout()
--- 機械学習/教師なし学習アルゴリズム.py
+++ 機械学習/教師なし学習アルゴリズム.py
@@ -6,36 +6,71 @@ from sklearn.model_selection import train_test_split
 import mglearn
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import MinMaxScaler # スケール変換器
+from sklearn.svm import SVC
 
-# データ変換の適用
+# # データ変換の適用
 cancer = load_breast_cancer()
-# 訓練セットとテストセットに分ける
-X_train, X_test, y_trsin, y_test = train_test_split(cancer.data, cancer.target, random_state=1)
-print(X_train.shape)
-print(X_test.shape)
+# # 訓練セットとテストセットに分ける
+# X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=1)
+# print(X_train.shape)
+# print(X_test.shape)
 
+# scaler = MinMaxScaler()
+# # スケール変換器のfitメソッドにはX_trainのみを与える
+# scaler.fit(X_train)
+
+# # データを変換
+# X_train_scaled = scaler.transform(X_train)
+# # スケール変換の前後のデータ特性をプリント
+# # 返還後の配列の形
+# print("transformed shape: {}".format(X_train_scaled.shape))
+# # 変換前の各特徴量の最小値
+# print("per-feature minimum before scaling:\n {}".format(X_train.min(axis=0)))
+# # 変換前の各特徴量の最大値
+# print("per-feature maximum before scaling:\n {}".format(X_train.max(axis=0)))
+# # 変換後の各特徴量の最小値
+# print("per-feature minimum after scaling:\n {}".format(X_train_scaled.min(axis=0)))
+# # 変換後の各特徴量の最大値
+# print("per-feature maximum after scaling:\n {}".format(X_train_scaled.max(axis=0)))
+
+# # テストデータを変換
+# X_test_scaled = scaler.transform(X_test)
+# # スケール変換の前後のデータ特性をプリント
+# # 変換後の各特徴量の最小値
+# print("per-feature minimum after scaling:\n{}".format(X_test_scaled.min(axis=0)))
+# # 変換後の各特徴量の最大値
+# print("per-feature maximum after scaling:\n{}".format(X_test_scaled.max(axis=0)))
+
+#-------------------------------------------------------------------------------------------------
+# SVCの学習に対するMimMaxScalerの効果
+# 今までやってきたSVCの学習
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
+svm = SVC(C=100)
+svm.fit(X_train, y_train)
+print("Test set accuraty:{:.2f}".format(svm.score(X_test, y_test)))
+
+# 0-1スケール変換で前処理
 scaler = MinMaxScaler()
-# スケール変換器のfitメソッドにはX_trainのみを与える
 scaler.fit(X_train)
-
-# データを変換
 X_train_scaled = scaler.transform(X_train)
-# スケール変換の前後のデータ特性をプリント
-# 返還後の配列の形
-print("transformed shape: {}".format(X_train_scaled.shape))
-# 変換前の各特徴量の最小値
-print("per-feature minimum before scaling:\n {}".format(X_train.min(axis=0)))
-# 変換前の各特徴量の最大値
-print("per-feature maximum before scaling:\n {}".format(X_train.max(axis=0)))
-# 変換後の各特徴量の最小値
-print("per-feature minimum after scaling:\n {}".format(X_train_scaled.min(axis=0)))
-# 変換後の各特徴量の最大値
-print("per-feature maximum after scaling:\n {}".format(X_train_scaled.max(axis=0)))
-
-# テストデータを変換
 X_test_scaled = scaler.transform(X_test)
-# スケール変換の前後のデータ特性をプリント
-# 変換後の各特徴量の最小値
-print("per-feature minimum after scaling:\n{}".format(X_test_scaled.min(axis=0)))
-# 変換後の各特徴量の最大値
-print("per-feature maximum after scaling:\n{}".format(X_test_scaled.max(axis=0)))
+
+# 変換された訓練データで学習
+svm.fit(X_train_scaled, y_train)
+
+# 変換されたテストセットでスコア計算
+print("Scaled test set accuracy: {:.2f}".format(
+    svm.score(X_test_scaled, y_test)))
+
+# 平均を0に分散を1に前処理
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# 変換された訓練データで学習
+svm.fit(X_train_scaled, y_train)
+# 変換されたテストセットでスコア計算
+print("SVM test accuracy: {:.2f}".format(svm.score(X_test_scaled, y_test)))
+
